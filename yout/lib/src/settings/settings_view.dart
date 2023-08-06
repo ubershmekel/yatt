73423a48595ed9a6bfea4d61139c32019d697241
Yuval Greenfield <ubershmekel@gmail.com>
2023-08-06 09:11:29 -0700

    Reselect languages to work with

--- yout/lib/src/settings/settings_view.dart
+++ yout/lib/src/settings/settings_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yout/src/language_select/language_item_list_view.dart';
 
 import 'settings_controller.dart';
 
@@ -19,32 +20,50 @@ class SettingsView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Settings'),
       ),
-      body: Padding(
+      body: ListView(
         padding: const EdgeInsets.all(16),
         // Glue the SettingsController to the theme selection DropdownButton.
         //
         // When a user selects a theme from the dropdown list, the
         // SettingsController is updated, which rebuilds the MaterialApp.
-        child: DropdownButton<ThemeMode>(
-          // Read the selected themeMode from the controller
-          value: controller.themeMode,
-          // Call the updateThemeMode method any time the user selects a theme.
-          onChanged: controller.updateThemeMode,
-          items: const [
-            DropdownMenuItem(
-              value: ThemeMode.system,
-              child: Text('System Theme'),
-            ),
-            DropdownMenuItem(
-              value: ThemeMode.light,
-              child: Text('Light Theme'),
-            ),
-            DropdownMenuItem(
-              value: ThemeMode.dark,
-              child: Text('Dark Theme'),
-            )
-          ],
-        ),
+        children: [
+          DropdownButton<ThemeMode>(
+            // Read the selected themeMode from the controller
+            value: controller.themeMode,
+            // Call the updateThemeMode method any time the user selects a theme.
+            onChanged: controller.updateThemeMode,
+            items: const [
+              DropdownMenuItem(
+                value: ThemeMode.system,
+                child: Text('System Theme'),
+              ),
+              DropdownMenuItem(
+                value: ThemeMode.light,
+                child: Text('Light Theme'),
+              ),
+              DropdownMenuItem(
+                value: ThemeMode.dark,
+                child: Text('Dark Theme'),
+              )
+            ],
+          ),
+          ListTile(
+              title: const Text('Choose your native language'),
+              onTap: () {
+                Navigator.restorablePushNamed(
+                  context,
+                  "/${LanguageItemListView.modeNative}",
+                );
+              }),
+          ListTile(
+              title: const Text('Choose your learning language'),
+              onTap: () {
+                Navigator.restorablePushNamed(
+                  context,
+                  "/${LanguageItemListView.modeLearn}",
+                );
+              }),
+        ],
       ),
     );
   }
