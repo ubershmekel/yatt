0dabaef55f047c74a1e8efdde73966dc5ddb8835
Yuval Greenfield <ubershmekel@gmail.com>
2023-08-11 20:26:38 -0700

    Rename language item->select

--- yout/lib/src/app.dart
+++ yout/lib/src/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:yout/src/language_select/language_item_list_view.dart';
+import 'package:yout/src/language_select/language_select_view.dart';
 import 'package:yout/src/settings/globals.dart';
 import 'package:yout/src/translate/translate_view.dart';
 
--- yout/lib/src/settings/settings_view.dart
+++ yout/lib/src/settings/settings_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:yout/src/language_select/language_item_list_view.dart';
+import 'package:yout/src/language_select/language_select_view.dart';
 
 import 'settings_controller.dart';
 
