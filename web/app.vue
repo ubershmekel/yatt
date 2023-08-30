a15753c5173488d7e9ae6db6ec86cde2a4841d8b
Yuval Greenfield <ubershmekel@gmail.com>
2023-08-29 22:22:44 -0700

    Simple front page and favicon

--- web/app.vue
+++ web/app.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <h1>You Are The Translator</h1>
+  <!-- Minified version -->
+  <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+  <link rel='icon' type='image/png' href='/favicon.png'>
+
+  <p>
+    Hands-free language learning.
+  </p>
+
+  <p>
+    Select your native language, then select a language to learn. The game will then give you a sentence to translate from
+    one language to the other.
+  </p>
+
+  <p>Learn English, Arabic, German, Greek, French, Hebrew, Italian, Japanese, Korean, Portuguese, Russian, or Spanish.
+  </p>
+
+  <p>Check out the code at <a href="https://github.com/ubershmekel/yatt">https://github.com/ubershmekel/yatt</a></p>
 </template>
