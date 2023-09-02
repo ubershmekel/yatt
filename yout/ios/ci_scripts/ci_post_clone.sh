3fd131f5ce841e050993de4d30d16a790bbfaca8
Yuval Greenfield <ubershmekel@gmail.com>
2023-09-01 23:12:08 -0700

    CD into the flutter directory, duh

--- yout/ios/ci_scripts/ci_post_clone.sh
+++ yout/ios/ci_scripts/ci_post_clone.sh
@@ -3,6 +3,8 @@
 # The default execution directory of this script is the ci_scripts directory.
 cd $CI_WORKSPACE # change working directory to the root of your cloned repo.
 
+cd yout
+
 # Install Flutter using git.
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
 export PATH="$PATH:$HOME/flutter/bin"
