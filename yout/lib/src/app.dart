e8a85ffece4f1b1c359dab0bbf8b94105be1945b
Yuval Greenfield <ubershmekel@gmail.com>
2023-09-08 17:57:40 -0700

    Remove "debug" banner from screenshots

--- yout/lib/src/app.dart
+++ yout/lib/src/app.dart
@@ -51,6 +51,10 @@ class MyApp extends StatelessWidget {
           // background.
           restorationScopeId: 'app',
 
+          // The "debug" banner showing up in screenshots got my app rejected
+          // in the ios app store review.
+          debugShowCheckedModeBanner: false,
+
           // Provide the generated AppLocalizations to the MaterialApp. This
           // allows descendant Widgets to display the correct translations
           // depending on the user's locale.
