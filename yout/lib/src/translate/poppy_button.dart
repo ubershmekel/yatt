71a3b97d5fda690e45f492d38bdeafe6301c2870
Yuval Greenfield <ubershmekel@gmail.com>
2023-09-13 22:25:00 -0700

    Slightly smaller buttons

--- yout/lib/src/translate/poppy_button.dart
+++ yout/lib/src/translate/poppy_button.dart
@@ -18,7 +18,7 @@ class PoppyButtonState extends State<PoppyButton> {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        height: 80.0,
+        height: 60.0,
         child: FittedBox(
             child: AnimatedOpacity(
                 opacity: _visible ? 1.0 : 0.5,
