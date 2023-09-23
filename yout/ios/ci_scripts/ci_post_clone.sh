e389679bd2e65d2034f4e9d160807f48a693bcca
Yuval Greenfield <ubershmekel@gmail.com>
2023-09-23 00:21:05 -0700

    Fix xcode cloud warning
    > Found usage of deprecated environment variable CI_WORKSPACE. Use CI_PRIMARY_REPOSITORY_PATH instead

--- yout/ios/ci_scripts/ci_post_clone.sh
+++ yout/ios/ci_scripts/ci_post_clone.sh
@@ -3,7 +3,7 @@
 set -e
 
 # The default execution directory of this script is the ci_scripts directory.
-cd $CI_WORKSPACE # change working directory to the root of your cloned repo.
+cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
 
 cd yout
 
